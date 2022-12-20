0b5588f0074d366b2792b3052572a7fb9bdfa204
Alexander Wang <zxcvb22217@gmail.com>
2022-12-20 18:51:34 +0800

    feat: move model downloading to the last step...
    so user can Ctrl-C if they don't want

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ git submodule init
 git submodule update
 echo "Installing dependencies and compiling C/C++ native addons..."
 npm install
-echo "Downloading whisper model..."
-bash whisper.cpp/models/download-ggml-model.sh tiny.en
 echo "Creating audio file for testing..."
 ffmpeg -i whisper.cpp/samples/jfk.wav -ar 16000 -ac 1 -c:a pcm_f32le \
 -f f32le whisper.cpp/samples/jfk.raw
+echo "Downloading whisper model..."
+bash whisper.cpp/models/download-ggml-model.sh tiny.en
